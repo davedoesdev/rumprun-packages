bacaea347b3854c7af758cc63bfd41b94bcacd81
David Halls <dahalls@gmail.com>
2016-04-04 16:40:24 +0100

    Package dep: curl

--- .docker-build/build-package.sh
+++ .docker-build/build-package.sh
@@ -4,6 +4,7 @@ if [ -z "$1" ]; then
   echo "No package specified"
   exit 1
 fi
+apt-get update -y && apt-get install -y curl
 cd "$(dirname "$0")/.."
 echo "RUMPRUN_TOOLCHAIN_TUPLE=$RUMPRUN_TOOLCHAIN_TUPLE" > config.mk
 cd "$1"
