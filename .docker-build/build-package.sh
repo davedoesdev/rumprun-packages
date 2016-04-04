18ef05f643ccf0c5760f49282518dc7639d8e809
David Halls <dahalls@gmail.com>
2016-04-04 09:40:35 +0100

    Add patch

--- .docker-build/build-package.sh
+++ .docker-build/build-package.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
   exit 1
 fi
 # install dependencies needed by packages but not by toolchain
-apk add --update tar
+apk add --update tar patch
 cd "$(dirname "$0")/.."
 echo "RUMPRUN_TOOLCHAIN_TUPLE=$RUMPRUN_TOOLCHAIN_TUPLE" > config.mk
 cd "$1"
