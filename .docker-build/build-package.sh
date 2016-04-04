98358749ce47778001c3bb9f307a4540bfda86ef
David Halls <dahalls@gmail.com>
2016-04-04 16:48:02 +0100

    Package dep: autoconf

--- .docker-build/build-package.sh
+++ .docker-build/build-package.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ]; then
   echo "No package specified"
   exit 1
 fi
-apt-get update -y && apt-get install -y curl
+apt-get update -y && apt-get install -y curl autoconf
 cd "$(dirname "$0")/.."
 echo "RUMPRUN_TOOLCHAIN_TUPLE=$RUMPRUN_TOOLCHAIN_TUPLE" > config.mk
 cd "$1"
