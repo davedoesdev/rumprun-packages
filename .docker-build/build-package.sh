4edd1fb39956bc1c9af39b8f5667750201c257a3
David Halls <dahalls@gmail.com>
2016-04-04 21:08:36 +0100

    Package dep: ruby2.0

--- .docker-build/build-package.sh
+++ .docker-build/build-package.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ]; then
   echo "No package specified"
   exit 1
 fi
-apt-get update -y && apt-get install -y curl autoconf genisoimage cmake python makefs bison
+apt-get update -y && apt-get install -y curl autoconf genisoimage cmake python makefs bison ruby2.0
 cd "$(dirname "$0")/.."
 echo "RUMPRUN_TOOLCHAIN_TUPLE=$RUMPRUN_TOOLCHAIN_TUPLE" > config.mk
 cd "$1"
