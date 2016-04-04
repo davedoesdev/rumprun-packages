1d2a0315b31e2dbfcc87c2a31be16ac194035750
David Halls <dahalls@gmail.com>
2016-04-04 13:50:26 +0100

    Not using apk any more

--- .docker-build/build-package.sh
+++ .docker-build/build-package.sh
@@ -4,8 +4,6 @@ if [ -z "$1" ]; then
   echo "No package specified"
   exit 1
 fi
-# install dependencies needed by packages but not by toolchain
-apk add --update tar patch
 cd "$(dirname "$0")/.."
 echo "RUMPRUN_TOOLCHAIN_TUPLE=$RUMPRUN_TOOLCHAIN_TUPLE" > config.mk
 cd "$1"
