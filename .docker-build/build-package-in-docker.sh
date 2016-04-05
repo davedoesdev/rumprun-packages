338d04df96306524c86e8396f8cff61f383fbe0e
David Halls <dahalls@gmail.com>
2016-04-05 18:41:12 +0100

    Reorg build scripts to make it easier to parameterize the build later build-package-in-docker.sh args can be used to change image and docker args (e.g. to pass down env vars)

--- .docker-build/build-package-in-docker.sh
+++ .docker-build/build-package-in-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-image="${2:-davedoesdev/rumprun-toolchain-x86_64-rumprun-netbsd-hw}"
-exec docker run --rm=true -e "BUILD_TAG=$BUILD_TAG" -v "$PWD/..:/rumprun-packages" "$image" "$1"
+exec docker run --rm=true \
+                -v "$(dirname "$0")/..:/rumprun-packages" \
+                "${@:-davedoesdev/rumprun-toolchain-x86_64-rumprun-netbsd-hw}" \
+                "$PACKAGE"
