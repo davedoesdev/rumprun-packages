d011bd1ce4ae9a644ca931a6122ae12737fc50f0
David Halls <dahalls@gmail.com>
2016-04-05 09:08:41 +0100

    Export BUILD_TAG to build container On Travis, set BUILD_TAG to TRAVIS_TAG

--- .docker-build/build-package-in-docker.sh
+++ .docker-build/build-package-in-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 image="${2:-davedoesdev/rumprun-toolchain-x86_64-rumprun-netbsd-hw}"
-exec docker run --rm=true -v "$PWD/..:/rumprun-packages" "$image" "$1"
+exec docker run --rm=true -e "BUILD_TAG=$BUILD_TAG" -v "$PWD/..:/rumprun-packages" "$image" "$1"
