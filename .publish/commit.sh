4409e9bcf7cee0b6ae56f0f98b26a3b2900f16a4
David Halls <dahalls@gmail.com>
2016-04-11 07:47:42 +0100

    Echo the commands we would run on publish

--- .publish/commit.sh
+++ .publish/commit.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo COMMIT "$@"
+# usage: commit.sh <repo-name>...
+export DTUF_REPOSITORIES_ROOT="$(dirname "$0")/dtuf_repos"
+echo "$DTUF_REPOSITORIES_ROOT"
+for repo in "$@"
+do
+  echo dtuf push-metadata "teksilo/$repo"
+done
--- .publish/push.sh
+++ .publish/push.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo PUSH "$@"
+# usage: push.sh <repo-name> <version> <file>
+export DTUF_REPOSITORIES_ROOT="$(dirname "$0")/dtuf_repos"
+echo "$DTUF_REPOSITORIES_ROOT"
+echo dtuf push-target "teksilo/$1" "$2-$RUMPRUN_TOOLCHAIN_TUPLE-$RUMPRUN_PUBLISH_CONFIG" "$3"
